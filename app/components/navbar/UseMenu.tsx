2890b6cd0cc362a358c92984ebf41a1f90c2a42a
yinguzwg-1 <976108364@qq.com>
2024-07-01 15:21:22 +0800

    feat: navbar ui

---
+++ app/components/navbar/UseMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+const UseMenu = () => {
+  return (
+    <div
+      className="
+      relative
+    "
+    >
+      <div className="flex flex-row items-center gap-3">
+        <div
+          className="
+            hidden
+            md:block
+            text-sm
+            font-semibold
+            py-3
+            px-4
+            rounded-full
+            hover:bg-neutral-100
+            transition
+            cursor-pointer
+          "
+          onClick={() => {}}
+        >
+          Airbnb your home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseMenu;
