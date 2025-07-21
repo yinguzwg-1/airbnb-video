c50382aa153f1e6c57816c7c3c2baf78776f7790
candace <candace-fe@gate.me>
2025-07-21 18:33:43 +0800

    fix: websocker 链接3

--- app/config/index.ts
+++ app/config/index.ts
@@ -16,6 +16,6 @@ console.log('Environment check:', {
 });
 
 export const config = {
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (isProduction() ? 'http://223.4.248.176:3001' : 'http://localhost:3001'),
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || (isProduction() ? 'https://223.4.248.176:3001' : 'http://localhost:3001'),
   NEXT_PUBLIC_APP_ID: '9527',
 }
