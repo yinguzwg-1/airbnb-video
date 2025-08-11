e6661c60d85c14105d65e8ab6607f5935a87094f
candace <candace-fe@gate.me>
2025-08-11 22:14:48 +0800

    fix: no cache

--- app/[lang]/music/page.tsx
+++ app/[lang]/music/page.tsx
@@ -3,25 +3,17 @@ import { Language } from "../../i18n";
 import { musicService } from '@/app/services/musicService';
 import { PlayerProvider } from '@/app/components/Common/MusicPlayerContext';
 import MusicHomePage from '@/app/components/MusicHomePage';
-import { Music } from "@/app/types/music";
+import { unstable_noStore as noStore } from 'next/cache';
 interface MusicPageProps {
-  params: { musicList: Music[], lang: Language };
+  params: { lang: Language };
 }
 
-export default function MusicPage({ params }: MusicPageProps) {
- 
+export default async function MusicPage({ params }: MusicPageProps) {
+  // 禁用缓存，强制每次请求都获取新数据
+  noStore();
+  const { musicList } = await musicService.getMusicList();
  
   return (<PlayerProvider>
-    <MusicHomePage musicList={params.musicList} lang={params.lang} />
+    <MusicHomePage musicList={musicList} lang={params.lang} />
   </PlayerProvider>);
 } 
-
-export async function getServerSideProps({ params }: MusicPageProps) {
-  const { musicList } = await musicService.getMusicList();
-  return {
-    props: {
-      musicList,
-      lang: params.lang
-    }
-  }
-}
