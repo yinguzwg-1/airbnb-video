7bd8bf6ae57d74ca303be6237a1611aa05b86d7a
candace <candace-fe@gate.me>
2025-08-11 22:08:36 +0800

    fix: 首页数据缓存

--- app/[lang]/music/page.tsx
+++ app/[lang]/music/page.tsx
@@ -3,15 +3,25 @@ import { Language } from "../../i18n";
 import { musicService } from '@/app/services/musicService';
 import { PlayerProvider } from '@/app/components/Common/MusicPlayerContext';
 import MusicHomePage from '@/app/components/MusicHomePage';
+import { Music } from "@/app/types/music";
 interface MusicPageProps {
-  params: { lang: Language };
+  params: { musicList: Music[], lang: Language };
 }
 
-export default async function MusicPage({ params }: MusicPageProps) {
+export default function MusicPage({ params }: MusicPageProps) {
  
-  const { musicList } = await musicService.getMusicList();
  
   return (<PlayerProvider>
-    <MusicHomePage musicList={musicList} lang={params.lang} />
+    <MusicHomePage musicList={params.musicList} lang={params.lang} />
   </PlayerProvider>);
 } 
+
+export async function getServerSideProps({ params }: MusicPageProps) {
+  const { musicList } = await musicService.getMusicList();
+  return {
+    props: {
+      musicList,
+      lang: params.lang
+    }
+  }
+}
