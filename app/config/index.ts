3ee316c86d5cd6bf44e482ca49794b2710d89fd4
candace <candace-fe@gate.me>
2025-09-16 08:22:43 +0800

    fix: api

--- app/config/index.ts
+++ app/config/index.ts
@@ -8,6 +8,6 @@ const isProduction = () => {
 
 
 export const config = {
-  NEXT_PUBLIC_API_URL: isProduction() ? 'http://223.4.248.176:3001/api' : 'http://localhost:3000/api',
+  NEXT_PUBLIC_API_URL: isProduction() ? 'https://zwg.autos/api' : 'http://localhost:3000/api',
   NEXT_PUBLIC_APP_ID: '9527',
 }
